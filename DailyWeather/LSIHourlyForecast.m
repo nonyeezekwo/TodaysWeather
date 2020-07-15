b8ff7fdc5ba35dfcca039334252d7cc3e1edf912
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-07-15 15:06:53 -0700

    Added LSIHourlyForecast

---
+++ DailyWeather/LSIHourlyForecast.m
@@ -0,0 +1,105 @@
+//
+//  LSIHourlyForecast.m
+//  DailyWeather
+//
+//  Created by Enzo Jimenez-Soto on 7/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIHourlyForecast.h"
+
+@implementation LSIHourlyForecast
+
+- (instancetype)initWithTime:(NSDate *)time
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+           precipProbability:(NSNumber *)precipProbability
+             precipIntensity:(NSNumber *)precipIntensity
+                  precipType:(NSString *)precipType
+              temperatureLow:(NSNumber *)temperatureLow
+             temperatureHigh:(NSNumber *)temperatureHigh
+         apparentTemperature:(NSNumber *)apparentTemperature
+                    humidity:(NSNumber *)humidity
+                    pressure:(NSNumber *)pressure
+                   windSpeed:(NSNumber *)windSpeed
+                 windBearing:(NSNumber *)windBearing
+                     uvIndex:(NSNumber *)uvIndex {
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _precipProbability = precipProbability;
+        _precipIntensity = precipIntensity;
+        _precipType = [precipType copy];
+        _temperatureLow = temperatureLow;
+        _temperatureHigh = temperatureHigh;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary{
+    
+    NSDate *time = dictionary[@"time"];
+    NSString *summary = dictionary[@"summary"];
+    NSString *icon = dictionary[@"icon"];
+    NSNumber *precipProbability = dictionary[@"precipProbability"];
+    NSNumber *sunriseTime = dictionary[@"sunriseTime"];
+    NSNumber *sunsetTime = dictionary[@"sunsetTime"];
+    NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+    NSString *precipType = dictionary[@"precipType"];
+    NSNumber *temperatureLow = dictionary[@"temperatureLow"];
+    NSNumber *temperatureHigh = dictionary[@"temperatureHigh"];
+    NSNumber *apparentTemperature = dictionary[@"apparentTemperature"];
+    NSNumber *humidity = dictionary[@"humidity"];
+    NSNumber *pressure = dictionary[@"pressure"];
+    NSNumber *windSpeed = dictionary[@"windSpeed"];
+    NSNumber *windBearing = dictionary[@"windBearing"];
+    NSNumber *uvIndex = dictionary[@"uvIndex"];
+    
+    
+    
+    if (!(time &&
+          summary &&
+          icon &&
+          precipProbability &&
+          sunriseTime &&
+          sunsetTime &&
+          precipIntensity &&
+          precipType &&
+          temperatureLow &&
+          temperatureHigh &&
+          apparentTemperature &&
+          humidity &&
+          pressure &&
+          windSpeed &&
+          windBearing &&
+          uvIndex)) {
+        // This makes the initializer "failable"
+        return nil;
+    }
+    
+    return [self initWithTime:time
+                      summary:summary
+                         icon:icon
+            precipProbability:precipProbability
+              precipIntensity:precipIntensity
+                   precipType:precipType
+               temperatureLow:temperatureLow
+              temperatureHigh:temperatureHigh
+          apparentTemperature:apparentTemperature
+                     humidity:humidity
+                     pressure:pressure
+                    windSpeed:windSpeed
+                  windBearing:windBearing
+                      uvIndex:uvIndex];
+    
+    
+}
+@end
